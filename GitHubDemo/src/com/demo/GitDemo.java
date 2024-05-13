3641aa333038fc971a39483ce55a46abcc433b60
RakhiKhadse <rakhikhadse009@gmail.com>
2024-05-13 15:50:15 +0545

    by rakhi

--- GitHubDemo/src/com/demo/GitDemo.java
+++ GitHubDemo/src/com/demo/GitDemo.java
@@ -2,6 +2,10 @@ package com.demo;
 
 public class GitDemo {
 
+	public void m1()
+	{
+		System.out.println("m1 Method");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome To GitHub Demo");
 	}
